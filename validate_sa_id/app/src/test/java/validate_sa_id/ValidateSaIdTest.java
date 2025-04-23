84eaccd3b6d8396b40ca49615259d37c6811498b
Pelma Monere Mahlobogoane <125448084+1632Pelma@users.noreply.github.com>
2025-04-23 11:13:54 +0200

    Created a static method called ValidateSaId that returns a boolean value if whether true or false the ID number that should be validated is true or false. Imported packages for the initial test with JUnit 5 annotations
    Created a static method called ValidateSaId that returns a boolean value if whether true or false the ID number that should be validated is true or false. Imported packages for the initial test with JUnit 5 annotations

--- validate_sa_id/app/src/test/java/validate_sa_id/ValidateSaIdTest.java
+++ validate_sa_id/app/src/test/java/validate_sa_id/ValidateSaIdTest.java
@@ -1,5 +1,12 @@
 package validate_sa_id;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ValidateSaIdTest {
-    
+    @Test
+    public void testValidIdNumbers() {
+        assertTrue(ValidateSaId.isIdNumberValid("2001014800086"), "ID 2001014800086 should be valid");
+        assertTrue(ValidateSaId.isIdNumberValid("2909035800085"), "ID 2909035800085 should be valid");
+    }
 }
