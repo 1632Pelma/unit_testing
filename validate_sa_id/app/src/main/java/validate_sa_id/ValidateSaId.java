5686e0bc208ed2e422b9d2b4211fde6347fe7f77
Pelma Monere Mahlobogoane <125448084+1632Pelma@users.noreply.github.com>
2025-04-23 11:34:37 +0200

    Added checks for length and numeric content to check if whether or not the length is equal to 13 and that the string only contains numeric values

--- validate_sa_id/app/src/main/java/validate_sa_id/ValidateSaId.java
+++ validate_sa_id/app/src/main/java/validate_sa_id/ValidateSaId.java
@@ -2,6 +2,12 @@ package validate_sa_id;
 
 public class ValidateSaId {
     public static boolean isIdNumberValid(String idNumber) {
+        if (idNumber == null || idNumber.length() != 13) {
+            return false;
+        }
+        if (!idNumber.matches("\\d{13}")) {
+            return false;
+        }
         return idNumber.equals("2019060708050") || idNumber.equals("1980120512053");
     }
 }
